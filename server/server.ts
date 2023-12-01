cb6bb4ef843364d9b607a1c7f5850c7e6c81cb8d
hinwong1021@gmail.com <hinwong1021@gmail.com>
2023-12-01 19:13:59 +0800

    fix server.ts

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 app.use(sessionMiddleware);
 
-app.use("./account", accountRoutes);
+app.use("/account", accountRoutes);
 
 app.use((req, res, next) => {
   console.log(req.method, req.url);
